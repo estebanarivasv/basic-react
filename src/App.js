56936c995ef325a7b2f859bb0972edfcd53457ef
Esteban <estebanarivasv@gmail.com>
2022-07-05 10:17:55 -0300

    Initial React files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const name = "Esteban";
+  const day = "sunny"
+
   return (
+    // Only one parent element
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hola, {name}</p>
+      <p>Esta soleado hoy? {day ? 'Yes' : 'No'}</p>
     </div>
   );
 }
